40f8d2ef5f24fc46a1725c51eb14f36ee7042b0f
fotoply <norberg.niels@gmail.com>
2016-10-06 12:43:13 +0200

    Replace for loop with foreach and make import statements more general

--- src/dk/sdu/mmmi/opn/assignment2/server/ServerListenerCatalog.java
+++ src/dk/sdu/mmmi/opn/assignment2/server/ServerListenerCatalog.java
@@ -1,7 +1,6 @@
 package dk.sdu.mmmi.opn.assignment2.server;
 
-import dk.sdu.mmmi.opn.assignment2.common.AbstractCatalog;
-import dk.sdu.mmmi.opn.assignment2.common.Product;
+import dk.sdu.mmmi.opn.assignment2.common.*;
 
 import java.rmi.RemoteException;
 import java.util.Random;
@@ -22,8 +21,8 @@ public class ServerListenerCatalog extends AbstractCatalog {
     @Override
     protected void initializeStock() throws RemoteException {
         Random random = new Random();
-        for (int i = 0; i < PRODUCT_NAMES.length; i++)
-            stock.put(PRODUCT_NAMES[i], new ServerListenerEntry(new Product(PRODUCT_NAMES[i], random.nextInt(1000) / 100.0f), random.nextInt(10)));
+        for (String PRODUCT_NAME : PRODUCT_NAMES)
+            stock.put(PRODUCT_NAME, new ServerListenerEntry(new Product(PRODUCT_NAME, random.nextInt(1000) / 100.0f), random.nextInt(10)));
 
     }
 }
